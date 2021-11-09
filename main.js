7c96042afa377038a24ae3b91ef26941c283edc7
hanzel777 <ban8780@naver.com>
2021-11-09 10:46:09 +0900

    test1

---
+++ main.js
@@ -0,0 +1,38 @@
+const express = require('express');
+var session = require('express-session')
+const compression = require('compression');
+const FileStore = require('session-file-store')(session);
+
+const app = express();
+
+app.use(express.urlencoded({extended : false}));
+app.use(compression());
+app.use(session({
+    secret: 'asd54687zx#csw!',
+    resave: false,
+    saveUninitialized: true,
+    store:new FileStore()
+}));
+
+const passport = require('./lib/passport')(app);
+const mypageRouter = require('./routes/mypageRouter');
+const auth = require('./routes/auth')(passport);
+const index = require('./routes/index');
+
+
+app.use('/', index); //메인페이지
+
+app.use('/auth', auth); //로그인 및 회원가입
+
+app.use('/mypage', mypageRouter); //마이페이지
+
+app.use(function(request, response, next){
+    response.status(404).send("페이지를 찾지 못했습니다.");
+});
+
+app.use(function(error, request, response, next){
+    console.error(error.stack);
+    response.status(500).send("에러가 발생했습니다.");
+});
+
+app.listen(3000)
