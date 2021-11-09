d40f69d907587856a37df54abd1f2d586518f4c4
hanzel777 <74997443+hanzel777@users.noreply.github.com>
2021-11-09 13:31:47 +0900

    Delete cofiInfor.js

--- dev/cofiInfor.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    DBinfor: {
-        host     : 'localhost',
-        user     : 'root',
-        password : 'zjvld1234!',
-        database : 'coffing_database'
-    },
-    secret: 'asd54687zx#csw!'
-}
