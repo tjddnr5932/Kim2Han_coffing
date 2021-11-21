1398180c58d4e695ea873a4e13680032630ca5ca
tjddnr5932 <tjddnr5932@naver.com>
2021-11-22 06:22:59 +0900

    get_distance

--- routes/recommendRouter.js
+++ routes/recommendRouter.js
@@ -18,6 +18,30 @@ const db = mysql.createConnection(
 const geocoder = nodeGeocoder({
     provider : 'openstreetmap'});
 
+function getDistance(lat1, lon1, lat2, lon2) {              //현재위치와 사용자 사이의 거리구하기
+    if ((lat1 == lat2) && (lon1 == lon2)){
+      return 0;
+    }
+    var radLat1 = Math.PI * lat1 / 180;
+    var radLat2 = Math.PI * lat2 / 180;
+    var theta = lon1 - lon2;
+    var radTheta = Math.PI * theta / 180;
+    var dist = Math.sin(radLat1) * Math.sin(radLat2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radTheta);
+    if (dist > 1){
+        dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = dist * 180 / Math.PI;
+    dist = dist * 60 * 1.1515 * 1.609344 * 1000;
+    if (dist < 100) {
+      dist = Math.round(dist / 10) * 10;            //반올림
+    }
+    else {
+      dist = Math.round(dist / 100) * 100;          //반올림
+    }
+    return dist;                                    //dist의 단위는 m 이다.
+}
+
 
 router.post('/', function(request, response, next){
   fs.readdir('./lib/recommendPages', function(error, filelist){
